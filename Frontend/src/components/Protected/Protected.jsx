63a7fbe4419686ce339f72943a1bf477932bb183
hansrajathole <hansrajathole1@gmail.com>
2025-05-17 15:40:34 +0530

    feat: enhance Protected component with login modal for unauthenticated users

--- Frontend/src/components/Protected/Protected.jsx
+++ Frontend/src/components/Protected/Protected.jsx
@@ -1,19 +1,59 @@
-import React, { useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
-const Protected = ({children}) => {
-    const Navigate = useNavigate()
-    useEffect(() => {
-        const token = localStorage.getItem('token')
-        if (!token) {
-          alert("please login first")
-          Navigate('/login')
-        }
-    }, [Navigate])
-    
-    
-  return (
-    children
-  )
-}
+import React, { useEffect, useState } from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+import { useNavigate } from "react-router-dom";
 
-export default Protected
+const Protected = ({ children }) => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [isLoginModalOpen, setIsLoginModalOpen] = useState(false);
+  const navigate = useNavigate();
+  const token = localStorage.getItem("token");
+  useEffect(() => {
+    if (!token) {
+      setIsAuthenticated(false);
+      setIsLoginModalOpen(true); // Open login modal
+    } else {
+      setIsAuthenticated(true);
+    }
+  }, [token]);
+
+  const handleLogin = () => {
+    setIsLoginModalOpen(false);
+    navigate("/login");
+  };
+
+
+  // const handleClose = () => {
+  //   setIsLoginModalOpen(false);
+  //   navigate("/");
+  // }
+  if (isAuthenticated) {
+    return children;
+  }
+
+
+
+  return (
+    <Dialog open={isLoginModalOpen} >
+      <DialogContent className="sm:max-w-md">
+        <DialogHeader>
+          <DialogTitle>Login Required</DialogTitle>
+          <DialogDescription>
+            You must be logged in to access this page. Please login to continue.
+          </DialogDescription>
+        </DialogHeader>
+        <div className="flex justify-end mt-4">
+          <Button onClick={handleLogin}>Go to Login</Button>
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default Protected;
